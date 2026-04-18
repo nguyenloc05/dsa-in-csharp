4e1c2f6a3f86eef0cee0a244440fb8c25d1c37ad
NguyenLoc2005 <nxl522005@gmail.com>
2026-04-18 21:20:17 +0700

    Two sum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+class TwoSum
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3 };
+        int target = 4;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    Console.WriteLine("Index:" + i + "," + j);
+                }
+            }
+        }
+    }
+}
