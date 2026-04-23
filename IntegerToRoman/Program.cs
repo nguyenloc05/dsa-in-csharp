569220beac8ce4c06bf0dfb07ca144e6303a732d
NguyenLoc2005 <nxl522005@gmail.com>
2026-04-23 15:50:51 +0700

    IntegerToRoman

---
+++ IntegerToRoman/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+class IntegerToRoman
+{
+    static void Main()
+    {
+        int x = 16;
+        string result = "";
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string[] romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+
+
+        for (int i = 0; i < values.Length; i++)
+        {
+           while(x>=values[i])
+            {
+                result += romans[i];
+                x -= values[i];
+            }
+
+        }
+        Console.WriteLine(result);
+    }
+}
